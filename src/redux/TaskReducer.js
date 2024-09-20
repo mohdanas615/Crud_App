604b87586a173fbe9443f0974ed7c2666efa0aaf
Mohd Anas Ansari <mohdanas615@gmail.com>
2024-09-20 16:36:17 +0530

    worked with redux

---
+++ src/redux/TaskReducer.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { taskList } from "../data";
+
+const taskSlice=createSlice({
+    name:'tasks',
+    initialState:taskList,
+    reducers:{
+ addTask:(state,action)=>{
+   state.push(action.payload)
+    console.log(action)
+ },
+ editTask:(state,action)=>{
+   const {id,title,description, due_date, priority, status}=action.payload;
+   const editingTask=state.find(task=>task.id==id);
+   if(editingTask){
+      editingTask.title=title;
+      editingTask.description=description;
+      editingTask.due_date=due_date;
+      editingTask.priority=priority;
+      editTask.status=status;
+   }
+
+ },
+ deleteTask:(state,action)=>{
+   const {id}=action.payload;
+   const existingTask=state.find(task=>task.id==id);
+   if(existingTask){
+      return state.filter(f=>f.id !==id);
+   }
+ }
+    }
+})
+export const {addTask, editTask, deleteTask}= taskSlice.actions;
+export default taskSlice.reducer;
