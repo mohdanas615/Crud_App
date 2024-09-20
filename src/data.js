9c8464eb2c1a5f2438e71606be48598fed8b9a29
Mohd Anas Ansari <mohdanas615@gmail.com>
2024-09-20 16:35:50 +0530

    added dummy data

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const taskList=[
+    {
+     title:"HTML",
+     description:"This is the structure of the website",
+     due_date:"2024/10/12",
+     priority:"Medium",
+     status:"completed",
+    //  id:new Date()
+    id:1
+    },
+    {
+     title:"CSS",
+     description:"This is the makeup of the website",
+     due_date:"2024/09/09",
+     priority:"High",
+     status:"In progress",
+    //  id:new Date()
+    id:2
+    },
+    {
+     title:"HTML",
+     description:"This is the brain of the website",
+     due_date:"2024/10/13",
+     priority:"Low",
+     status:"In progress",
+    //  id:new Date()
+    id:3
+    }
+]
