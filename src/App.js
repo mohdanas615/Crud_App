8d75a45799e13a77716b8097c993ef594e789e15
Mohd Anas Ansari <mohdanas615@gmail.com>
2024-09-20 16:38:51 +0530

    added routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './component/Home';
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import AddTask from './component/AddTask';
+import EditTask from './component/EditTask';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <BrowserRouter>
+       <Routes>
+         <Route path="/" element={<Home/>} />
+       </Routes>
+       <Routes>
+         <Route path="/AddTask" element={<AddTask/>} />
+       </Routes>
+       <Routes>
+         <Route path="/EditTask/:id" element={<EditTask/>} />
+       </Routes>
+       
+     </BrowserRouter>
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Provider} from 'react-redux';
+import {configureStore} from '@reduxjs/toolkit';
+import TaskReducer from './redux/TaskReducer';
+
+
+const store=configureStore({
+  reducer:{
+    tasks:TaskReducer
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( 
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
+   
   </React.StrictMode>
 );
 
